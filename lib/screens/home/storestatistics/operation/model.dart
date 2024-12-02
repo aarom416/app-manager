8ae4ba7bf0e3f3d8c379bde5891eb8c84a5dbd23
voyagercaptain <voyagersoft2022@gmail.com>
2024-12-02 19:18:33 +0900

    [Feat] 홈 > 사업자 정보 > 통계 조회

---
+++ lib/screens/home/storestatistics/operation/model.dart
@@ -0,0 +1,46 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'model.freezed.dart';
+part 'model.g.dart';
+
+enum StoreStatisticsStatus {
+  init,
+  success,
+  error,
+}
+
+@freezed
+class MenuRecommendStatisticsDTO with _$MenuRecommendStatisticsDTO {
+  const factory MenuRecommendStatisticsDTO({
+    @Default(0) int menuId,
+    @Default('') String createdDate, //": "2024년 11월 25일"
+  }) = _MenuRecommendStatisticsDTO;
+
+  factory MenuRecommendStatisticsDTO.fromJson(Map<String, dynamic> json) =>
+      _$MenuRecommendStatisticsDTOFromJson(json);
+}
+
+@freezed
+class OrderInformationStatisticsDTO with _$OrderInformationStatisticsDTO {
+  const factory OrderInformationStatisticsDTO({
+    @Default('') String receivedFoodType,
+    @Default('') String completedDate, //": "2024년 11월 25일"
+  }) = _OrderInformationStatisticsDTO;
+
+  factory OrderInformationStatisticsDTO.fromJson(Map<String, dynamic> json) =>
+      _$OrderInformationStatisticsDTOFromJson(json);
+}
+
+@freezed
+abstract class StoreStatisticsModel with _$StoreStatisticsModel {
+  const factory StoreStatisticsModel({
+    @Default([])
+    List<MenuRecommendStatisticsDTO> menuRecommendStatisticsDTOList, // 대표자 구분
+    @Default([])
+    List<OrderInformationStatisticsDTO>
+        orderInformationStatisticsDTOList, // 사업자 이름
+  }) = _StoreStatisticsModel;
+
+  factory StoreStatisticsModel.fromJson(Map<String, dynamic> json) =>
+      _$StoreStatisticsModelFromJson(json);
+}
