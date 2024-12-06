f1a917575086aafa3de0bbae09a7b39260c4ca5f
voyagercaptain <voyagersoft2022@gmail.com>
2024-12-06 19:48:14 +0900

    [Feat] 홈 > 패키지 이동

--- lib/screens/home/storesettlement/email/screen.dart
+++ lib/screens/home/storesettlement/email/screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:singleeat/core/components/action_button.dart';
 import 'package:singleeat/core/components/app_bar_with_left_arrow.dart';
 import 'package:singleeat/core/components/container.dart';
@@ -8,19 +9,32 @@ import 'package:singleeat/core/components/spacing.dart';
 import 'package:singleeat/core/components/text_field_wrapper.dart';
 import 'package:singleeat/core/components/typography.dart';
 import 'package:singleeat/core/constants/colors.dart';
+import 'package:singleeat/screens/home/storesettlement/operation/provider.dart';
 
-class SendEmailScreen extends StatelessWidget {
-  final TextEditingController emailController = TextEditingController(text: "singleat@singleat.com");
+class SendEmailScreen extends ConsumerStatefulWidget {
+  const SendEmailScreen({super.key});
+
+  @override
+  ConsumerState<SendEmailScreen> createState() => _SendEmailScreen();
+}
+
+class _SendEmailScreen extends ConsumerState<SendEmailScreen> {
+  final TextEditingController emailController = TextEditingController(text: "");
 
   @override
   Widget build(BuildContext context) {
+    final state = ref.watch(storeSettlementNotifierProvider);
+    final provider = ref.read(storeSettlementNotifierProvider.notifier);
+
     return Scaffold(
         appBar: AppBarWithLeftArrow(title: "이메일 발송"),
         body: SGContainer(
-            padding: EdgeInsets.symmetric(horizontal: SGSpacing.p4, vertical: SGSpacing.p6),
+            padding: EdgeInsets.symmetric(
+                horizontal: SGSpacing.p4, vertical: SGSpacing.p6),
             width: double.infinity,
             color: Color(0xFFFAFAFA),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               SGTypography.label("받는사람"),
               SizedBox(height: SGSpacing.p4),
               SGTextFieldWrapper(
@@ -32,7 +46,9 @@ class SendEmailScreen extends StatelessWidget {
                       contentPadding: EdgeInsets.all(SGSpacing.p4),
                       isCollapsed: true,
                       hintStyle: TextStyle(color: SGColors.gray3),
-                      border: const OutlineInputBorder(borderRadius: BorderRadius.zero, borderSide: BorderSide.none),
+                      border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.zero,
+                          borderSide: BorderSide.none),
                     )),
               )),
               Expanded(
@@ -41,19 +57,24 @@ class SendEmailScreen extends StatelessWidget {
                 children: [
                   SGActionButton(
                       onPressed: () {
+                        provider.onChangeEmail(email: emailController.text);
                         showSGDialog(
                             context: context,
                             childrenBuilder: (ctx) => [
                                   Center(
-                                    child: SGTypography.body("사장님의 이메일로\n전송해드리겠습니다.",
+                                    child: SGTypography.body(
+                                        "사장님의 이메일로\n전송해드리겠습니다.",
                                         size: FontSize.medium,
                                         weight: FontWeight.w700,
                                         align: TextAlign.center,
                                         lineHeight: 1.25),
                                   ),
                                   SizedBox(height: SGSpacing.p3),
-                                  SGTypography.body("받는사람: ${emailController.text}",
-                                      size: FontSize.small, weight: FontWeight.w500, color: SGColors.gray4),
+                                  SGTypography.body(
+                                      "받는사람: ${emailController.text}",
+                                      size: FontSize.small,
+                                      weight: FontWeight.w500,
+                                      color: SGColors.gray4),
                                   SizedBox(height: SGSpacing.p5),
                                   Row(
                                     children: [
@@ -64,8 +85,10 @@ class SendEmailScreen extends StatelessWidget {
                                           },
                                           child: SGContainer(
                                             color: SGColors.gray3,
-                                            borderRadius: BorderRadius.circular(SGSpacing.p3),
-                                            padding: EdgeInsets.all(SGSpacing.p4),
+                                            borderRadius: BorderRadius.circular(
+                                                SGSpacing.p3),
+                                            padding:
+                                                EdgeInsets.all(SGSpacing.p4),
                                             child: Center(
                                               child: SGTypography.body("취소",
                                                   size: FontSize.normal,
@@ -75,17 +98,21 @@ class SendEmailScreen extends StatelessWidget {
                                           ),
                                         ),
                                       ),
-                                      SizedBox(width: SGSpacing.p2 + SGSpacing.p05),
+                                      SizedBox(
+                                          width: SGSpacing.p2 + SGSpacing.p05),
                                       Expanded(
                                         child: GestureDetector(
                                           onTap: () {
                                             Navigator.of(ctx).pop();
-                                            Navigator.of(context).pop();
+                                            //Navigator.of(context).pop();
+                                            provider.generateSettlementReport();
                                           },
                                           child: SGContainer(
                                             color: SGColors.primary,
-                                            borderRadius: BorderRadius.circular(SGSpacing.p3),
-                                            padding: EdgeInsets.all(SGSpacing.p4),
+                                            borderRadius: BorderRadius.circular(
+                                                SGSpacing.p3),
+                                            padding:
+                                                EdgeInsets.all(SGSpacing.p4),
                                             child: Center(
                                               child: SGTypography.body("확인",
                                                   size: FontSize.normal,
