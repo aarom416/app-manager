2b6ffb0f2e754434606f0454355dfa7a2eb9b4da
voyagercaptain <voyagersoft2022@gmail.com>
2024-12-16 21:30:36 +0900

    [Fix] 가게 최소 주문 금액 데이터(min_delivery_price)

--- lib/screens/home/storemanagement/delivery/screen.dart
+++ lib/screens/home/storemanagement/delivery/screen.dart
@@ -32,7 +32,8 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
   Widget build(BuildContext context) {
     final DeliveryState state = ref.watch(deliveryNotifierProvider);
 
-    final DeliveryNotifier provider = ref.read(deliveryNotifierProvider.notifier);
+    final DeliveryNotifier provider =
+        ref.read(deliveryNotifierProvider.notifier);
 
     return SingleChildScrollView(
       child: Container(
@@ -44,7 +45,10 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    SGTypography.body("배달 예상 시간", color: SGColors.black, size: FontSize.normal, weight: FontWeight.w600),
+                    SGTypography.body("배달 예상 시간",
+                        color: SGColors.black,
+                        size: FontSize.normal,
+                        weight: FontWeight.w600),
                     SizedBox(width: SGSpacing.p2),
                     InkWell(
                         onTap: () {
@@ -52,16 +56,21 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
                               builder: (context) => DeliveryTimeScreen(
                                     minDeliveryTime: state.minDeliveryTime,
                                     maxDeliveryTime: state.maxDeliveryTime,
-                                    onSaveFunction: (minDeliveryTime, maxDeliveryTime) {
+                                    onSaveFunction:
+                                        (minDeliveryTime, maxDeliveryTime) {
                                       // print("onSaveFunction $minDeliveryTime $maxDeliveryTime");
-                                      provider.updateDeliveryTime(minDeliveryTime, maxDeliveryTime);
+                                      provider.updateDeliveryTime(
+                                          minDeliveryTime, maxDeliveryTime);
                                     },
                                   )));
                         },
                         child: const Icon(Icons.edit, size: FontSize.small)),
                   ],
                 ),
-                SGTypography.body("${state.minDeliveryTime} ~ ${state.maxDeliveryTime}분", color: SGColors.gray4, size: FontSize.normal)
+                SGTypography.body(
+                    "${state.minDeliveryTime} ~ ${state.maxDeliveryTime}분",
+                    color: SGColors.gray4,
+                    size: FontSize.normal)
               ])
             ]),
 
@@ -73,7 +82,10 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    SGTypography.body("픽업 예상 시간", color: SGColors.black, size: FontSize.normal, weight: FontWeight.w600),
+                    SGTypography.body("픽업 예상 시간",
+                        color: SGColors.black,
+                        size: FontSize.normal,
+                        weight: FontWeight.w600),
                     SizedBox(width: SGSpacing.p2),
                     InkWell(
                         onTap: () {
@@ -81,16 +93,21 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
                               builder: (context) => TakeOutTimeScreen(
                                     minTakeOutTime: state.minTakeOutTime,
                                     maxTakeOutTime: state.maxTakeOutTime,
-                                    onSaveFunction: (minTakeOutTime, maxTakeOutTime) {
+                                    onSaveFunction:
+                                        (minTakeOutTime, maxTakeOutTime) {
                                       // print("onSaveFunction $minTakeOutTime $maxTakeOutTime");
-                                      provider.updatePickupTime(minTakeOutTime, maxTakeOutTime);
+                                      provider.updatePickupTime(
+                                          minTakeOutTime, maxTakeOutTime);
                                     },
                                   )));
                         },
                         child: const Icon(Icons.edit, size: FontSize.small)),
                   ],
                 ),
-                SGTypography.body("${state.minTakeOutTime} ~ ${state.maxTakeOutTime}분", color: SGColors.gray4, size: FontSize.normal)
+                SGTypography.body(
+                    "${state.minTakeOutTime} ~ ${state.maxTakeOutTime}분",
+                    color: SGColors.gray4,
+                    size: FontSize.normal)
               ])
             ]),
 
@@ -102,66 +119,120 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
                 borderRadius: BorderRadius.circular(SGSpacing.p4),
                 color: SGColors.white,
                 borderColor: SGColors.line2,
-                child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => DeliveryTipScreen(
-                                baseDeliveryTip: state.baseDeliveryTip,
-                                deliveryTipMax: state.deliveryTipMax,
-                                minimumOrderPrice: state.minimumOrderPrice,
-                                storeDeliveryTipDTOList: state.storeDeliveryTipDTOList,
-                                deliveryTipInfo: state.deliveryDataModel.deliveryTipInfo,
-                                onSaveFunction: (baseDeliveryTip, minimumOrderPrice, storeDeliveryTipDTOList) {
-                                  // print("onSaveFunction $baseDeliveryTip $minimumOrderPrice $storeDeliveryTipDTOList");
-                                  provider.updateDeliveryTip(baseDeliveryTip, minimumOrderPrice, storeDeliveryTipDTOList);
-                                },
-                              )));
-                    },
-                    child: Row(children: [
-                      SGTypography.body("기본 배달팁", color: SGColors.black, size: FontSize.normal, weight: FontWeight.w600),
-                      SizedBox(width: SGSpacing.p2),
-                      Icon(Icons.edit, size: FontSize.small),
-                    ]),
-                  ),
-                  SizedBox(height: SGSpacing.p5),
-                  SGTypography.body("최소 주문 금액", color: SGColors.gray4, weight: FontWeight.w600),
-                  SizedBox(height: SGSpacing.p5 / 2),
-                  Table(border: TableBorder.all(color: SGColors.line2, borderRadius: BorderRadius.circular(SGSpacing.p3)), children: [
-                    TableRow(children: [
-                      Center(child: SGContainer(padding: EdgeInsets.all(SGSpacing.p3 + SGSpacing.p05), child: SGTypography.body("주문 금액", color: SGColors.black, weight: FontWeight.w700))),
-                      Center(child: SGContainer(padding: EdgeInsets.all(SGSpacing.p3 + SGSpacing.p05), child: SGTypography.body("배달팁", color: SGColors.gray5, weight: FontWeight.w500))),
-                    ]),
-                    TableRow(children: [
-                      Center(
-                          child: SGContainer(
-                              padding: EdgeInsets.all(SGSpacing.p3 + SGSpacing.p05),
-                              child: SGTypography.body("${state.minimumOrderPrice.toKoreanCurrency}원 이상", color: SGColors.black, weight: FontWeight.w700))),
-                      Center(
-                          child: SGContainer(
-                              padding: EdgeInsets.all(SGSpacing.p3 + SGSpacing.p05),
-                              child: SGTypography.body("${state.baseDeliveryTip.toKoreanCurrency}원", color: SGColors.gray5, weight: FontWeight.w500))),
-                    ])
-                  ]),
-                  SizedBox(height: SGSpacing.p4),
-                  // if (state.storeDeliveryTipDTOList.isNotEmpty) ...[
-                  SGTypography.body("배달팁 추가", color: SGColors.gray4, weight: FontWeight.w600),
-                  SizedBox(height: SGSpacing.p5 / 2),
-                  Table(border: TableBorder.all(color: SGColors.line2, borderRadius: BorderRadius.circular(SGSpacing.p3)), children: [
-                    ...state.storeDeliveryTipDTOList.map((deliveryTipModel) => TableRow(children: [
-                          Center(
-                              child: SGContainer(
-                                  padding: EdgeInsets.all(SGSpacing.p3 + SGSpacing.p05),
-                                  child: SGTypography.body("${deliveryTipModel.minPrice.toKoreanCurrency}원 ~ ${deliveryTipModel.maxPrice.toKoreanCurrency}원",
-                                      color: SGColors.black, weight: FontWeight.w700))),
-                          Center(
-                              child: SGContainer(
-                                  padding: EdgeInsets.all(SGSpacing.p3 + SGSpacing.p05),
-                                  child: SGTypography.body("${deliveryTipModel.deliveryTip.toKoreanCurrency}원", color: SGColors.gray5, weight: FontWeight.w500))),
-                        ])),
-                  ]),
-                  // ],
-                ])),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => DeliveryTipScreen(
+                                    baseDeliveryTip: state.baseDeliveryTip,
+                                    deliveryTipMax: state.deliveryTipMax,
+                                    minimumOrderPrice: state.minimumOrderPrice,
+                                    storeDeliveryTipDTOList:
+                                        state.storeDeliveryTipDTOList,
+                                    deliveryTipInfo:
+                                        state.deliveryDataModel.deliveryTipInfo,
+                                    onSaveFunction: (baseDeliveryTip,
+                                        minimumOrderPrice,
+                                        storeDeliveryTipDTOList) {
+                                      // print("onSaveFunction $baseDeliveryTip $minimumOrderPrice $storeDeliveryTipDTOList");
+                                      provider.updateDeliveryTip(
+                                          baseDeliveryTip,
+                                          minimumOrderPrice,
+                                          storeDeliveryTipDTOList);
+                                    },
+                                  )));
+                        },
+                        child: Row(children: [
+                          SGTypography.body("기본 배달팁",
+                              color: SGColors.black,
+                              size: FontSize.normal,
+                              weight: FontWeight.w600),
+                          SizedBox(width: SGSpacing.p2),
+                          Icon(Icons.edit, size: FontSize.small),
+                        ]),
+                      ),
+                      SizedBox(height: SGSpacing.p5),
+                      SGTypography.body("최소 주문 금액",
+                          color: SGColors.gray4, weight: FontWeight.w600),
+                      SizedBox(height: SGSpacing.p5 / 2),
+                      Table(
+                          border: TableBorder.all(
+                              color: SGColors.line2,
+                              borderRadius:
+                                  BorderRadius.circular(SGSpacing.p3)),
+                          children: [
+                            TableRow(children: [
+                              Center(
+                                  child: SGContainer(
+                                      padding: EdgeInsets.all(
+                                          SGSpacing.p3 + SGSpacing.p05),
+                                      child: SGTypography.body("주문 금액",
+                                          color: SGColors.black,
+                                          weight: FontWeight.w700))),
+                              Center(
+                                  child: SGContainer(
+                                      padding: EdgeInsets.all(
+                                          SGSpacing.p3 + SGSpacing.p05),
+                                      child: SGTypography.body("배달팁",
+                                          color: SGColors.gray5,
+                                          weight: FontWeight.w500))),
+                            ]),
+                            ...state.storeDeliveryTipDTOList
+                                .map((deliveryTipModel) => TableRow(children: [
+                                      Center(
+                                          child: SGContainer(
+                                              padding: EdgeInsets.all(
+                                                  SGSpacing.p3 + SGSpacing.p05),
+                                              child: SGTypography.body(
+                                                  "${deliveryTipModel.minPrice.toKoreanCurrency}원",
+                                                  color: SGColors.black,
+                                                  weight: FontWeight.w700))),
+                                      Center(
+                                          child: SGContainer(
+                                              padding: EdgeInsets.all(
+                                                  SGSpacing.p3 + SGSpacing.p05),
+                                              child: SGTypography.body(
+                                                  "${deliveryTipModel.deliveryTip.toKoreanCurrency}원",
+                                                  color: SGColors.gray5,
+                                                  weight: FontWeight.w500))),
+                                    ])),
+                            // ]
+                          ]),
+                      SizedBox(height: SGSpacing.p4),
+                      // if (state.storeDeliveryTipDTOList.isNotEmpty) ...[
+                      SGTypography.body("배달팁 추가",
+                          color: SGColors.gray4, weight: FontWeight.w600),
+                      SizedBox(height: SGSpacing.p5 / 2),
+                      Table(
+                          border: TableBorder.all(
+                              color: SGColors.line2,
+                              borderRadius:
+                                  BorderRadius.circular(SGSpacing.p3)),
+                          children: [
+                            ...state.storeDeliveryTipDTOList
+                                .map((deliveryTipModel) => TableRow(children: [
+                                      Center(
+                                          child: SGContainer(
+                                              padding: EdgeInsets.all(
+                                                  SGSpacing.p3 + SGSpacing.p05),
+                                              child: SGTypography.body(
+                                                  "${deliveryTipModel.minPrice.toKoreanCurrency}원 ~ ${deliveryTipModel.maxPrice.toKoreanCurrency}원",
+                                                  color: SGColors.black,
+                                                  weight: FontWeight.w700))),
+                                      Center(
+                                          child: SGContainer(
+                                              padding: EdgeInsets.all(
+                                                  SGSpacing.p3 + SGSpacing.p05),
+                                              child: SGTypography.body(
+                                                  "${deliveryTipModel.deliveryTip.toKoreanCurrency}원",
+                                                  color: SGColors.gray5,
+                                                  weight: FontWeight.w500))),
+                                    ])),
+                          ]),
+                      // ],
+                    ])),
 
             SizedBox(height: SGSpacing.p3),
 
@@ -182,17 +253,16 @@ class _DeliveryScreenState extends ConsumerState<DeliveryScreen> {
                     ),
                     Flexible(
                       child: SGTypography.body(
-                        "현재는 ${state.deliveryAddress}에서만 배달 가능해요.",
-                        color: SGColors.gray4,
-                        size: FontSize.normal,
-                        align: TextAlign.end
-                      ),
+                          "현재는 ${state.deliveryAddress}에서만 배달 가능해요.",
+                          color: SGColors.gray4,
+                          size: FontSize.normal,
+                          align: TextAlign.end),
                     ),
                   ],
                 ),
               ],
             ),
-      ],
+          ],
         ),
       ),
     );
