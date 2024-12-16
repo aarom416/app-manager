c613b59aa8fee3bc4098a5559d2cd2de8611ea61
jblee <voyagersoft2023@gmail.com>
2024-12-16 21:53:14 +0900

    [Update] 배달 예상 시간 변경 API 경로 수정

--- lib/screens/home/storemanagement/delivery/service.dart
+++ lib/screens/home/storemanagement/delivery/service.dart
@@ -14,9 +14,12 @@ class DeliveryService {
   DeliveryService(this.ref);
 
   /// GET - 배달/포장 정보 조회
-  Future<Response<dynamic>> getDeliveryTakeoutInfo({required String storeId}) async {
+  Future<Response<dynamic>> getDeliveryTakeoutInfo(
+      {required String storeId}) async {
     try {
-      return ref.read(requestApiProvider).get(path: RestApiUri.getDeliveryTakeoutInfo.replaceAll('{storeId}', storeId));
+      return ref.read(requestApiProvider).get(
+          path: RestApiUri.getDeliveryTakeoutInfo
+              .replaceAll('{storeId}', storeId));
     } on DioException catch (e) {
       logger.e("DioException: ${e.message}");
       return Future.error(e);
@@ -27,11 +30,18 @@ class DeliveryService {
   }
 
   /// POST - 배달 예상 시간 수정
-  Future<Response<dynamic>> updateDeliveryTime({required String storeId, required int minDeliveryTime, required int maxDeliveryTime}) async {
+  Future<Response<dynamic>> updateDeliveryTime(
+      {required String storeId,
+      required int minDeliveryTime,
+      required int maxDeliveryTime}) async {
     try {
       return await ref.read(requestApiProvider).post(
-        RestApiUri.updateOperationTime,
-        data: {'storeId': UserHive.getBox(key: UserKey.storeId), 'minTime': minDeliveryTime, 'maxTime': maxDeliveryTime},
+        RestApiUri.updateDeliveryTime,
+        data: {
+          'storeId': UserHive.getBox(key: UserKey.storeId),
+          'minTime': minDeliveryTime,
+          'maxTime': maxDeliveryTime
+        },
       );
     } on DioException catch (e) {
       logger.e("DioException: ${e.message}");
@@ -43,11 +53,18 @@ class DeliveryService {
   }
 
   /// POST - 포장 예상 시간 수정
-  Future<Response<dynamic>> updatePickupTime({required String storeId, required int minTakeOutTime, required int maxTakeOutTime}) async {
+  Future<Response<dynamic>> updatePickupTime(
+      {required String storeId,
+      required int minTakeOutTime,
+      required int maxTakeOutTime}) async {
     try {
       return await ref.read(requestApiProvider).post(
         RestApiUri.updatePickupTime,
-        data: {'storeId': UserHive.getBox(key: UserKey.storeId), 'minTime': minTakeOutTime, 'maxTime': maxTakeOutTime},
+        data: {
+          'storeId': UserHive.getBox(key: UserKey.storeId),
+          'minTime': minTakeOutTime,
+          'maxTime': maxTakeOutTime
+        },
       );
     } on DioException catch (e) {
       logger.e("DioException: ${e.message}");
@@ -83,7 +100,11 @@ class DeliveryService {
         ]
       }
    */
-  Future<Response<dynamic>> updateDeliveryTip({required String storeId, required int baseDeliveryTip, required int minimumOrderPrice, required List<DeliveryTipModel> storeDeliveryTipDTOList}) async {
+  Future<Response<dynamic>> updateDeliveryTip(
+      {required String storeId,
+      required int baseDeliveryTip,
+      required int minimumOrderPrice,
+      required List<DeliveryTipModel> storeDeliveryTipDTOList}) async {
     try {
       return await ref.read(requestApiProvider).post(
         RestApiUri.updatePickupTime,
@@ -115,4 +136,5 @@ class DeliveryService {
   }
 }
 
-final deliveryServiceProvider = Provider<DeliveryService>((ref) => DeliveryService(ref));
+final deliveryServiceProvider =
+    Provider<DeliveryService>((ref) => DeliveryService(ref));
