8cebaf9646296ca0db68b1c17f2b4e0d0e6bbc2e
voyagercaptain <voyagersoft2022@gmail.com>
2024-12-17 16:59:55 +0900

    [Fix] 옵션개수 버튼 활성 비활성 처리

--- lib/core/screens/numeric_range_edit_screen.dart
+++ lib/core/screens/numeric_range_edit_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:singleeat/core/components/snackbar.dart';
+
 import '../components/action_button.dart';
 import '../components/container.dart';
 import '../components/numeric_textfield.dart';
@@ -8,23 +10,8 @@ import '../components/spacing.dart';
 import '../components/typography.dart';
 import '../constants/colors.dart';
 
-class NumericRangeEditScreen extends StatelessWidget {
-  final String title;
-  final String description;
-  final String minLabel;
-  final String maxLabel;
-  final String unitLabel;
-  final bool allowMinZero;
-  final bool allowMaxZero;
-  final int maxMinValue;
-  final int maxMaxValue;
-  final bool hideMinInput;
-  final int minValue;
-  final int maxValue;
-  final String confirmButtonLabel;
-  final Function(int, int) onConfirm;
-
-  const NumericRangeEditScreen({
+class NumericRangeEditScreen extends ConsumerStatefulWidget {
+  NumericRangeEditScreen({
     super.key,
     required this.title,
     required this.description,
@@ -43,9 +30,50 @@ class NumericRangeEditScreen extends StatelessWidget {
   });
 
   @override
+  ConsumerState<NumericRangeEditScreen> createState() =>
+      _NumericRangeEditScreen();
+
+  final String title;
+  final String description;
+  final String minLabel;
+  final String maxLabel;
+  final String unitLabel;
+  final bool allowMinZero;
+  final bool allowMaxZero;
+  final int maxMinValue;
+  final int maxMaxValue;
+  final bool hideMinInput;
+  final int minValue;
+  final int maxValue;
+  final String confirmButtonLabel;
+  final Function(int, int) onConfirm;
+}
+
+class _NumericRangeEditScreen extends ConsumerState<NumericRangeEditScreen> {
+  /*const NumericRangeEditScreen({
+    super.key,
+    required this.title,
+    required this.description,
+    this.minLabel = "최소",
+    this.maxLabel = "최대",
+    this.unitLabel = "개",
+    this.allowMinZero = false,
+    this.allowMaxZero = false,
+    this.maxMinValue = 99999,
+    this.maxMaxValue = 99999,
+    this.hideMinInput = false,
+    this.minValue = 0,
+    this.maxValue = 0,
+    this.confirmButtonLabel = "변경하기",
+    required this.onConfirm,
+  });*/
+  bool isEnable = false;
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController minValueController = TextEditingController(text: minValue.toString());
-    final TextEditingController maxValueController = TextEditingController(text: maxValue.toString());
+    final TextEditingController minValueController =
+        TextEditingController(text: widget.minValue.toString());
+    final TextEditingController maxValueController =
+        TextEditingController(text: widget.maxValue.toString());
 
     String minErrorMessage = "";
     String maxErrorMessage = "";
@@ -57,19 +85,19 @@ class NumericRangeEditScreen extends StatelessWidget {
       final minValue = int.tryParse(minValueController.text) ?? 0;
       final maxValue = int.tryParse(maxValueController.text) ?? 0;
 
-      if (!allowMinZero && minValue == 0) {
+      if (!widget.allowMinZero && minValue == 0) {
         minErrorMessage = "0은 입력할 수 없습니다.";
-      } else if (minValue > maxMinValue) {
-        minErrorMessage = "최대 $maxMinValue까지 입력할 수 있습니다.";
+      } else if (minValue > widget.maxMinValue) {
+        minErrorMessage = "최대 ${widget.maxMinValue}까지 입력할 수 있습니다.";
       }
 
-      if (!allowMaxZero && maxValue == 0) {
+      if (!widget.allowMaxZero && maxValue == 0) {
         maxErrorMessage = "0은 입력할 수 없습니다.";
-      } else if (maxValue > maxMaxValue) {
-        maxErrorMessage = "최대 $maxMaxValue까지 입력할 수 있습니다.";
+      } else if (maxValue > widget.maxMaxValue) {
+        maxErrorMessage = "최대 ${widget.maxMaxValue}까지 입력할 수 있습니다.";
       }
 
-      if (!hideMinInput && minValue > maxValue) {
+      if (!widget.hideMinInput && minValue > maxValue) {
         minErrorMessage = "최소값은 최대값보다 클 수 없습니다.";
       }
     }
@@ -78,7 +106,10 @@ class NumericRangeEditScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: SGColors.white,
         elevation: 0,
-        title: SGTypography.body(title, size: FontSize.medium, weight: FontWeight.w700, color: SGColors.black),
+        title: SGTypography.body(widget.title,
+            size: FontSize.medium,
+            weight: FontWeight.w700,
+            color: SGColors.black),
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: SGColors.black),
           onPressed: () => Navigator.of(context).pop(),
@@ -95,31 +126,36 @@ class NumericRangeEditScreen extends StatelessWidget {
             if (minErrorMessage.isEmpty && maxErrorMessage.isEmpty) {
               final minValue = int.tryParse(minValueController.text) ?? 0;
               final maxValue = int.tryParse(maxValueController.text) ?? 0;
-              onConfirm(minValue, maxValue);
+              widget.onConfirm(minValue, maxValue);
               showGlobalSnackBar(context, "성공적으로 변경되었습니다.");
             } else {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  content: Text(minErrorMessage.isNotEmpty ? minErrorMessage : maxErrorMessage),
+                  content: Text(minErrorMessage.isNotEmpty
+                      ? minErrorMessage
+                      : maxErrorMessage),
                   backgroundColor: Colors.red,
                 ),
               );
             }
           },
-          disabled: minValueController.text == "0" || maxValueController.text == "0",
-          label: confirmButtonLabel,
+          disabled: isEnable,
+          label: widget.confirmButtonLabel,
         ),
       ),
       body: SGContainer(
         borderWidth: 0,
         color: const Color(0xFFFAFAFA),
-        padding: EdgeInsets.symmetric(horizontal: SGSpacing.p4, vertical: SGSpacing.p6),
+        padding: EdgeInsets.symmetric(
+            horizontal: SGSpacing.p4, vertical: SGSpacing.p6),
         child: ListView(
           children: [
-            SGTypography.body(description, weight: FontWeight.w700, size: FontSize.normal),
+            SGTypography.body(widget.description,
+                weight: FontWeight.w700, size: FontSize.normal),
             SizedBox(height: SGSpacing.p4),
-            if (!hideMinInput) ...[
-              SGTypography.body(minLabel, size: FontSize.small, color: SGColors.gray4),
+            if (!widget.hideMinInput) ...[
+              SGTypography.body(widget.minLabel,
+                  size: FontSize.small, color: SGColors.gray4),
               SizedBox(height: SGSpacing.p2 + SGSpacing.p05),
               SGContainer(
                 color: Colors.white,
@@ -129,25 +165,36 @@ class NumericRangeEditScreen extends StatelessWidget {
                   children: [
                     Expanded(
                       child: NumericTextField(
-                        initialValue: minValue,
-                        maxLength: maxMinValue.toString().length,
+                        initialValue: widget.minValue,
+                        maxLength: widget.maxMinValue.toString().length,
                         onValueChanged: (value) {
-                          minValueController.text = value.toString();
+                          setState(() {
+                            minValueController.text = value.toString();
+                            if (minValueController.text == '0' ||
+                                maxValueController.text == '0') {
+                              isEnable = true;
+                            } else {
+                              isEnable = false;
+                            }
+                          });
                         },
                       ),
                     ),
-                    SGTypography.body(unitLabel, size: FontSize.small, color: SGColors.gray4),
+                    SGTypography.body(widget.unitLabel,
+                        size: FontSize.small, color: SGColors.gray4),
                     SizedBox(width: SGSpacing.p4),
                   ],
                 ),
               ),
               if (minErrorMessage.isNotEmpty) ...[
                 SizedBox(height: SGSpacing.p2),
-                SGTypography.body(minErrorMessage, size: FontSize.small, color: Colors.red),
+                SGTypography.body(minErrorMessage,
+                    size: FontSize.small, color: Colors.red),
               ],
               SizedBox(height: SGSpacing.p3),
             ],
-            SGTypography.body(maxLabel, size: FontSize.small, color: SGColors.gray4),
+            SGTypography.body(widget.maxLabel,
+                size: FontSize.small, color: SGColors.gray4),
             SizedBox(height: SGSpacing.p2 + SGSpacing.p05),
             SGContainer(
               color: Colors.white,
@@ -157,21 +204,31 @@ class NumericRangeEditScreen extends StatelessWidget {
                 children: [
                   Expanded(
                     child: NumericTextField(
-                      initialValue: maxValue,
-                      maxLength: maxMaxValue.toString().length,
+                      initialValue: widget.maxValue,
+                      maxLength: widget.maxMaxValue.toString().length,
                       onValueChanged: (value) {
-                        maxValueController.text = value.toString();
+                        setState(() {
+                          maxValueController.text = value.toString();
+                          if (minValueController.text == '0' ||
+                              maxValueController.text == '0') {
+                            isEnable = true;
+                          } else {
+                            isEnable = false;
+                          }
+                        });
                       },
                     ),
                   ),
-                  SGTypography.body(unitLabel, size: FontSize.small, color: SGColors.gray4),
+                  SGTypography.body(widget.unitLabel,
+                      size: FontSize.small, color: SGColors.gray4),
                   SizedBox(width: SGSpacing.p4),
                 ],
               ),
             ),
             if (maxErrorMessage.isNotEmpty) ...[
               SizedBox(height: SGSpacing.p2),
-              SGTypography.body(maxErrorMessage, size: FontSize.small, color: Colors.red),
+              SGTypography.body(maxErrorMessage,
+                  size: FontSize.small, color: Colors.red),
             ],
           ],
         ),
