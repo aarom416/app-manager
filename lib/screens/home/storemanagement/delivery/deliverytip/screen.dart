d619838a18a8c7b395d085ae9501a20ea97b1a16
holywater <holywater@voyagersoft.co.kr>
2024-12-17 10:24:37 +0900

    배달팁 수정

--- lib/screens/home/storemanagement/delivery/deliverytip/screen.dart
+++ lib/screens/home/storemanagement/delivery/deliverytip/screen.dart
@@ -25,7 +25,14 @@ class DeliveryTipScreen extends StatefulWidget {
   final String deliveryTipInfo;
   final Function(int, int, List<DeliveryTipModel>) onSaveFunction;
 
-  const DeliveryTipScreen({super.key, required this.baseDeliveryTip, required this.deliveryTipMax, required this.minimumOrderPrice, required this.storeDeliveryTipDTOList, required this.deliveryTipInfo, required this.onSaveFunction});
+  const DeliveryTipScreen(
+      {super.key,
+      required this.baseDeliveryTip,
+      required this.deliveryTipMax,
+      required this.minimumOrderPrice,
+      required this.storeDeliveryTipDTOList,
+      required this.deliveryTipInfo,
+      required this.onSaveFunction});
 
   @override
   State<DeliveryTipScreen> createState() => _DeliveryTipScreenState();
@@ -52,7 +59,12 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
         context: context,
         childrenBuilder: (ctx) => [
               if (subTitle.isEmpty) ...[
-                Center(child: SGTypography.body(mainTitle, size: FontSize.medium, weight: FontWeight.w700, lineHeight: 1.25, align: TextAlign.center)),
+                Center(
+                    child: SGTypography.body(mainTitle,
+                        size: FontSize.medium,
+                        weight: FontWeight.w700,
+                        lineHeight: 1.25,
+                        align: TextAlign.center)),
                 SizedBox(height: SGSpacing.p6),
                 GestureDetector(
                   onTap: () {
@@ -64,13 +76,28 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                     borderColor: SGColors.primary,
                     padding: EdgeInsets.symmetric(vertical: SGSpacing.p5),
                     borderRadius: BorderRadius.circular(SGSpacing.p3),
-                    child: Center(child: SGTypography.body("확인", color: SGColors.white, weight: FontWeight.w700, size: FontSize.normal)),
+                    child: Center(
+                        child: SGTypography.body("확인",
+                            color: SGColors.white,
+                            weight: FontWeight.w700,
+                            size: FontSize.normal)),
                   ),
                 )
               ] else ...[
-                Center(child: SGTypography.body(mainTitle, size: FontSize.medium, weight: FontWeight.w700, lineHeight: 1.25, align: TextAlign.center)),
+                Center(
+                    child: SGTypography.body(mainTitle,
+                        size: FontSize.medium,
+                        weight: FontWeight.w700,
+                        lineHeight: 1.25,
+                        align: TextAlign.center)),
                 SizedBox(height: SGSpacing.p4),
-                Center(child: SGTypography.body(subTitle, color: SGColors.gray4, size: FontSize.small, weight: FontWeight.w700, lineHeight: 1.25, align: TextAlign.center)),
+                Center(
+                    child: SGTypography.body(subTitle,
+                        color: SGColors.gray4,
+                        size: FontSize.small,
+                        weight: FontWeight.w700,
+                        lineHeight: 1.25,
+                        align: TextAlign.center)),
                 SizedBox(height: SGSpacing.p6),
                 GestureDetector(
                   onTap: () {
@@ -82,7 +109,11 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                     borderColor: SGColors.primary,
                     padding: EdgeInsets.symmetric(vertical: SGSpacing.p5),
                     borderRadius: BorderRadius.circular(SGSpacing.p3),
-                    child: Center(child: SGTypography.body("확인", color: SGColors.white, weight: FontWeight.w700, size: FontSize.normal)),
+                    child: Center(
+                        child: SGTypography.body("확인",
+                            color: SGColors.white,
+                            weight: FontWeight.w700,
+                            size: FontSize.normal)),
                   ),
                 )
               ]
@@ -94,12 +125,30 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
     return Scaffold(
       appBar: AppBarWithLeftArrow(title: "배달팁 수정"),
       floatingActionButton: Container(
-          constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width - SGSpacing.p8, maxHeight: 58),
+          constraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width - SGSpacing.p8,
+              maxHeight: 58),
           child: SGActionButton(
             onPressed: () {
               FocusScope.of(context).unfocus();
-              if (baseDeliveryTip > widget.deliveryTipMax || storeDeliveryTipDTOList.any((deliveryTipModel) => deliveryTipModel.deliveryTip > widget.deliveryTipMax)) {
-                showFailDialogWithImage(mainTitle: "배달팁 등록 실패", subTitle: "배달팁은 ${widget.deliveryTipMax.toKoreanCurrency}원 이하만 설정 가능합니다.");
+
+              for (int i = storeDeliveryTipDTOList.length - 1; i > 0; i--) {
+                if (storeDeliveryTipDTOList[i].minPrice !=
+                    storeDeliveryTipDTOList[i - 1].maxPrice) {
+                  showFailDialogWithImage(
+                      mainTitle: "배달팁 등록 실패",
+                      subTitle: "현재 최대 주문 금액이 다음\n최소 주문 금액과 같아야 합니다.");
+                  return;
+                }
+              }
+
+              if (baseDeliveryTip > widget.deliveryTipMax ||
+                  storeDeliveryTipDTOList.any((deliveryTipModel) =>
+                      deliveryTipModel.deliveryTip > widget.deliveryTipMax)) {
+                showFailDialogWithImage(
+                    mainTitle: "배달팁 등록 실패",
+                    subTitle:
+                        "배달팁은 ${widget.deliveryTipMax.toKoreanCurrency}원 이하만 설정 가능합니다.");
               } else {
                 /*
                     todo storeDeliveryTipDTOList 의 입력값 validation 필요
@@ -109,22 +158,33 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                     배달팁 내용, 최소 가격, 최대 가겨, 배달 팁 리스트의 크기는 모두 같고 인덱스 순으로 같은 배달팁입니다.
                     배달팁 변경 페이지에 존재하는 배달팁에 대한 정보를 전송해야 합니다. (기존에 설정되어 있는 배달팁 포함)
                  */
-                widget.onSaveFunction(baseDeliveryTip, minimumOrderPrice, storeDeliveryTipDTOList);
+                widget.onSaveFunction(baseDeliveryTip, minimumOrderPrice,
+                    storeDeliveryTipDTOList);
                 showGlobalSnackBar(context, "성공적으로 변경되었습니다.");
               }
             },
             label: "변경하기",
-            disabled: widget.minimumOrderPrice == minimumOrderPrice && widget.baseDeliveryTip == baseDeliveryTip && const DeepCollectionEquality().equals(widget.storeDeliveryTipDTOList, storeDeliveryTipDTOList),
+            disabled: widget.minimumOrderPrice == minimumOrderPrice &&
+                widget.baseDeliveryTip == baseDeliveryTip &&
+                const DeepCollectionEquality().equals(
+                    widget.storeDeliveryTipDTOList, storeDeliveryTipDTOList),
           )),
       body: SGContainer(
           color: const Color(0xFFFAFAFA),
-          padding: EdgeInsets.symmetric(horizontal: SGSpacing.p4, vertical: SGSpacing.p5),
+          padding: EdgeInsets.symmetric(
+              horizontal: SGSpacing.p4, vertical: SGSpacing.p5),
           child: ListView(children: [
             // --------------------------- 기본 배달팁 ---------------------------
-            SGTypography.body("기본 배달팁", color: SGColors.black, size: FontSize.normal, weight: FontWeight.w600),
+            SGTypography.body("기본 배달팁",
+                color: SGColors.black,
+                size: FontSize.normal,
+                weight: FontWeight.w600),
             SizedBox(height: SGSpacing.p3),
             MultipleInformationBox(children: [
-              SGTypography.body("최소 주문 금액 설정", color: SGColors.black, size: FontSize.small, weight: FontWeight.w600),
+              SGTypography.body("최소 주문 금액 설정",
+                  color: SGColors.black,
+                  size: FontSize.small,
+                  weight: FontWeight.w600),
               SizedBox(height: SGSpacing.p4),
               Row(children: [
                 Expanded(
@@ -137,8 +197,11 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                           decoration: InputDecoration(
                             hintText: "",
                             hintStyle: TextStyle(color: SGColors.gray4),
-                            contentPadding: EdgeInsets.all(SGSpacing.p4).copyWith(right: 0),
-                            border: const OutlineInputBorder(borderRadius: BorderRadius.zero, borderSide: BorderSide.none),
+                            contentPadding:
+                                EdgeInsets.all(SGSpacing.p4).copyWith(right: 0),
+                            border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.zero,
+                                borderSide: BorderSide.none),
                           ),
                           onValueChanged: (minimumOrderPrice) {
                             setState(() {
@@ -148,7 +211,13 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                           },
                         ),
                       ),
-                      SGContainer(padding: EdgeInsets.symmetric(horizontal: SGSpacing.p4), child: SGTypography.body("원 이상", color: SGColors.gray4, size: FontSize.small, weight: FontWeight.w500)),
+                      SGContainer(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: SGSpacing.p4),
+                          child: SGTypography.body("원 이상",
+                              color: SGColors.gray4,
+                              size: FontSize.small,
+                              weight: FontWeight.w500)),
                     ],
                   )),
                 ),
@@ -163,8 +232,11 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                           decoration: InputDecoration(
                             hintText: "",
                             hintStyle: TextStyle(color: SGColors.gray4),
-                            contentPadding: EdgeInsets.all(SGSpacing.p4).copyWith(right: 0),
-                            border: const OutlineInputBorder(borderRadius: BorderRadius.zero, borderSide: BorderSide.none),
+                            contentPadding:
+                                EdgeInsets.all(SGSpacing.p4).copyWith(right: 0),
+                            border: const OutlineInputBorder(
+                                borderRadius: BorderRadius.zero,
+                                borderSide: BorderSide.none),
                           ),
                           onValueChanged: (baseDeliveryTip) {
                             setState(() {
@@ -174,7 +246,13 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
                           },
                         ),
                       ),
-                      SGContainer(padding: EdgeInsets.symmetric(horizontal: SGSpacing.p4), child: SGTypography.body("원", color: SGColors.gray4, size: FontSize.small, weight: FontWeight.w500)),
+                      SGContainer(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: SGSpacing.p4),
+                          child: SGTypography.body("원",
+                              color: SGColors.gray4,
+                              size: FontSize.small,
+                              weight: FontWeight.w500)),
                     ],
                   )),
                 ),
@@ -184,12 +262,16 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
             SizedBox(height: SGSpacing.p5),
 
             // --------------------------- 배달팁 추가 ---------------------------
-            SGTypography.body("배달팁 추가", color: SGColors.black, size: FontSize.normal, weight: FontWeight.w600),
+            SGTypography.body("배달팁 추가",
+                color: SGColors.black,
+                size: FontSize.normal,
+                weight: FontWeight.w600),
             SizedBox(height: SGSpacing.p3),
             DeliveryTipCard(
               storeDeliveryTipDTOList: storeDeliveryTipDTOList,
               onEditFunction: (storeDeliveryTipDTOList) {
-                print("onEditFunction storeDeliveryTipDTOList $storeDeliveryTipDTOList");
+                print(
+                    "onEditFunction storeDeliveryTipDTOList $storeDeliveryTipDTOList");
                 setState(() {
                   this.storeDeliveryTipDTOList = storeDeliveryTipDTOList;
                 });
@@ -197,7 +279,8 @@ class _DeliveryTipScreenState extends State<DeliveryTipScreen> {
             ),
 
             SizedBox(height: SGSpacing.p2 + SGSpacing.p05),
-            SGTypography.body("* ${widget.deliveryTipInfo}", color: SGColors.gray3, lineHeight: 1.25),
+            SGTypography.body("* ${widget.deliveryTipInfo}",
+                color: SGColors.gray3, lineHeight: 1.25),
             SizedBox(height: SGSpacing.p20),
           ])),
     );
